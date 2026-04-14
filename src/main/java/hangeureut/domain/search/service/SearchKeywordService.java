ab307c3611f9027f160275e254458fbef594e4e5
mmiiduu <2171434@hansung.ac.kr>
2026-04-14 21:11:26 +0900

    feat: 전체 공개 앨범 조회 API 추가

--- src/main/java/hangeureut/domain/search/service/SearchKeywordService.java
+++ src/main/java/hangeureut/domain/search/service/SearchKeywordService.java
@@ -1,7 +1,12 @@
 package hangeureut.domain.search.service;
 
 import hangeureut.domain.search.web.dto.SearchKeywordResponseDTO;
+import java.util.List;
 
 public interface SearchKeywordService {
     SearchKeywordResponseDTO.SearchResultDTO searchKeyword(String keyword);
+    
+    // 추가
+    List<SearchKeywordResponseDTO.AlbumInfo> getAllPublicAlbums();
+    List<SearchKeywordResponseDTO.ReviewInfo> getAllPublicReviews();
 }
